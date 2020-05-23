6df7ee762e1c86abfbf392cfd4458ebe6d85b358
Alexej <alexejdubow@gmail.com>
2020-05-23 22:40:26 +0300

    Updates

---
+++ precache-manifest.951c2a0df532feaa51d2da74451145b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5403ed24f1d4cdae986d9e0e9725b4b8",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "484c96ff4613473a3e9a",
+    "url": "/goit-react-hw-02-feedback/static/css/main.903b2bf4.chunk.css"
+  },
+  {
+    "revision": "45e819d3b6ecf70d8147",
+    "url": "/goit-react-hw-02-feedback/static/js/2.a1a00ad6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.a1a00ad6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484c96ff4613473a3e9a",
+    "url": "/goit-react-hw-02-feedback/static/js/main.9161ac67.chunk.js"
+  },
+  {
+    "revision": "530a2ea63a63b985e80c",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.f97b67d2.js"
+  }
+]);
